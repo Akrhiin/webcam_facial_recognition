75d16afe324b4a5c78b09ad29d119bc857e3e7ed
Michael Pace <70898410+Akrhiin@users.noreply.github.com>
2023-04-10 20:17:54 -0400

    Enhance frontal face detection

--- webcam_scan_face.py
+++ webcam_scan_face.py
@@ -4,16 +4,24 @@ import tkinter as tk
 from tkinter import simpledialog
 
 # load cascade classifier training file for haarcascade
-frontCascPath = "face_data/haarcascade_frontalface_default.xml"
-profilCascPath = "face_data/haarcascade_profileface.xml"
-frontFaceCascade = cv2.CascadeClassifier(frontCascPath)
-profilFaceCascade = cv2.CascadeClassifier(profilCascPath)
+# front view
+front_casc_path = "face_data/haarcascade_frontalface_default.xml"
+front_face_cascade = cv2.CascadeClassifier(front_casc_path)
+# front view alt
+front_alt_casc_path = "face_data/haarcascade_frontalface_alt.xml"
+front_alt_face_cascade = cv2.CascadeClassifier(front_alt_casc_path)
+# front view alt2
+front_alt2_casc_path = "face_data/haarcascade_frontalface_alt2.xml"
+front_alt2_face_cascade = cv2.CascadeClassifier(front_alt2_casc_path)
+# profile view
+profile_casc_path = "face_data/haarcascade_profileface.xml"
+profile_face_cascade = cv2.CascadeClassifier(profile_casc_path)
 
 # initialize camera
 cam = cv2.VideoCapture(0)
 
 # create window
-cv2.namedWindow("[ESC] to close, [SPACE] to save image")
+#cv2.namedWindow("[ESC] to close, [SPACE] to save image")
 
 while(True):
     # read frame
@@ -24,29 +32,59 @@ while(True):
     # convert to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # detect faces
-    frontFaces = frontFaceCascade.detectMultiScale(
+    front_faces = front_face_cascade.detectMultiScale(              # front view
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
-    profileFaces = profilFaceCascade.detectMultiScale(
+    front_alt_faces = front_alt_face_cascade.detectMultiScale(      # front view alt
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
+    front_alt2_faces = front_alt2_face_cascade.detectMultiScale(    # front view alt2
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    profile_faces = profile_face_cascade.detectMultiScale(          # profile view
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    
     # draw rectangle around faces
-    if len(frontFaces) > 0:
-        for (x, y, w, h) in frontFaces:
+    # front view
+    if len(front_faces) > 0:
+        #print("front face detected")       # *uncomment for debugging*
+        for (x, y, w, h) in front_faces:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    elif len(profileFaces) > 0:
-        for (x, y, w, h) in profileFaces:
+    # front view alt
+    elif len(front_alt_faces) > 0:
+        #print("front alt face detected")   # *uncomment for debugging*
+        for (x, y, w, h) in front_alt_faces:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # front view alt2
+    elif len(front_alt2_faces) > 0:
+        #print("front alt2 face detected")  # *uncomment for debugging*
+        for (x, y, w, h) in front_alt2_faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # profile view
+    elif len(profile_faces) > 0:
+        #print("profile face detected")     # *uncomment for debugging*
+        for (x, y, w, h) in profile_faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
     # display frame
-    cv2.imshow("cam_feed", frame)
+    cv2.imshow("[ESC] to close, [SPACE] to save image", frame)
     k = cv2.waitKey(1)      # on key press
     if k%256 == 27:             # ESC pressed
         print("closing...")
